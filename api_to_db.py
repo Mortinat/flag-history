9661bbb39b0a46690526761bc810cc780007c843
morti <qstevens@hotmail.be>
2021-11-02 13:05:46 +0100

    [ADD] first commit of script for save history flag

---
+++ api_to_db.py
@@ -0,0 +1,25 @@
+import requests
+import psycopg2
+import time
+import os
+
+conn = psycopg2.connect(
+    database=os.environ.get("SQL_DATABASE", "name"),
+    host=os.environ.get("SQL_HOST", "localhost"),
+    port=os.environ.get("SQL_PORT", "5432"),
+    user=os.environ.get("SQL_USER", "user"),
+    password=os.environ.get("SQL_PASSWORD", "password"))
+
+cur = conn.cursor()
+cur.execute('''CREATE TABLE IF NOT EXISTS number_pix
+               (total int, timestamp timestamp default current_timestamp)''')
+conn.commit()
+
+while(True):
+    time.sleep(30)
+    resp = requests.get(url="https://api-flag.fouloscopie.com/flag")
+    data = resp.json()
+
+    cur.execute(f"INSERT INTO number_pix VALUES ({len(data)})")
+
+    conn.commit()
---
+++ get_save.py
@@ -0,0 +1,30 @@
+import requests
+import psycopg2
+import time
+import datetime
+import os
+
+conn = psycopg2.connect(
+    database=os.environ.get("SQL_DATABASE", "name"),
+    host=os.environ.get("SQL_HOST", "localhost"),
+    port=os.environ.get("SQL_PORT", "5432"),
+    user=os.environ.get("SQL_USER", "user"),
+    password=os.environ.get("SQL_PASSWORD", "password"))
+cur = conn.cursor()
+cur.execute('''CREATE TABLE IF NOT EXISTS pix_history
+               (hexColor text, indexInFlag int, timestamp timestamp default current_timestamp)''')
+conn.commit()
+
+while(True):
+    url=f"https://api-flag.fouloscopie.com/flag/after/{(datetime.datetime.utcnow()).isoformat()}"
+
+    time.sleep(60)
+
+    resp = requests.get(url=url)
+    data = resp.json()
+
+    ti = time.time()
+    for line in data:
+        cur.execute(f"INSERT INTO number_pix VALUES (?,?,?)", (line['hexColor'], line['indexInFlag'], time.time()))
+    
+    conn.commit()
